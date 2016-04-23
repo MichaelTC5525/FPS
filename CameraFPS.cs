0c2b5b5871cc2d339190bbcdd8d4c590e6319166
MichaelTC5525 <michael.cheung@live.ca>
2016-04-23 02:03:19 -0700

    Update CameraFPS.cs

--- CameraFPS.cs
+++ CameraFPS.cs
@@ -1,14 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CameraFPS : MonoBehaviour {
 
     //Private variables can only be accessed from within the Behaviour script in the Integrated Development Environment.
     private Vector3 offset;
+	float lookHorizontal;
+	float lookVertical;
 
     //Public variables can be altered inside the Unity Editor. 
     public GameObject player;
-    public GameObject m_Camera;
 
     // Use this for initialization
     void Start () {
@@ -20,43 +21,31 @@ public class CameraFPS : MonoBehaviour {
     void LateUpdate () {
         //Change the camera's position according to the player's movement, so it keeps the same place in relation to the player GameObject.
         transform.position = player.transform.position + offset;
-		
+		print (transform.rotation);
     }
 
-    // Call the look-around function in FixedUpdate for a regular interval.
+    // Call the look-around functions in FixedUpdate for a regular interval.
     void FixedUpdate()
     {
-        Perspective();
+		//Call the functions responsible for the camera view movement.
+		MouseHorizontal();
+		MouseVertical();
+		float maxMin = transform.rotation.x;
 
-        //Clamp maximum and minimum values for looking up and down using the stored variable.
-        float upDown = transform.eulerAngles.x;
-        if (upDown >= 60f)
-        {
-            upDown = 60f;
-        }
-        else if (upDown <= 300f)
-        {
-            upDown = 300f;
-        }
-    }
-
-    void Perspective() {
-        //Call the functions responsible for the camera view movement.
-        MouseHorizontal();
-        MouseVertical();
+		maxMin = Mathf.Clamp (lookVertical, -0.5f, 0.5f);
     }
 
     void MouseHorizontal()
     {
-        float lookHorizontal = Input.GetAxis("Mouse X");
+		lookHorizontal = Input.GetAxis("Mouse X");
         transform.Rotate(Vector3.up, lookHorizontal, Space.World);  //Global y-axis rotation will be level, no matter how high or low the player is looking from.
     }
 
     void MouseVertical()
     {
-        //Use transform.eulerAngles to use Degrees, as transform.rotation is stored as a Quaternion.
-        float lookVertical = Input.GetAxis("Mouse Y");
+		lookVertical = Input.GetAxis ("Mouse Y");
         //Allow rotation on the local x-axis.
         transform.Rotate(Vector3.right, lookVertical, Space.Self);
     }
+
 }
